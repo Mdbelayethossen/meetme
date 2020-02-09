c79df13a728700c33895415edd284ce59b6e77a1
Mdbelayethossen <59694089+Mdbelayethossen@users.noreply.github.com>
2020-02-09 22:46:38 +0600

    update js

--- js/main.js
+++ js/main.js
@@ -69,6 +69,29 @@ $(document).ready(function () {
 
 
 	})(jQuery);
+	(function($){
+		"use strict"
+		var nav_offset_top = $('.header_area').height()+50; 
+		/*-------------------------------------------------------------------------------
+		  Navbar 
+		-------------------------------------------------------------------------------*/
+	
+		//* Navbar Fixed  
+		function navbarFixed(){
+			if ( $('.header_area').length ){ 
+				$(window).scroll(function() {
+					var scroll = $(window).scrollTop();   
+					if (scroll >= nav_offset_top ) {
+						$(".header_area").addClass("navbar_fixed");
+					} else {
+						$(".header_area").removeClass("navbar_fixed");
+					}
+				});
+			};
+		};
+		navbarFixed();
+	
+	})(jQuery)
 	/*---------gallery isotope js-----------*/
 	let $btns = $('.project_gallery .button-group button');
 
